4f2d5835b169ea4a48ac2b3299751e552ec71136
Ivan <ivanstecki99@gmail.com>
2023-06-11 02:59:20 -0300

    Ajustes de diseño

--- app/src/main/java/ar/edu/ort/respirar/ui/fragments/HomeFragment.kt
+++ app/src/main/java/ar/edu/ort/respirar/ui/fragments/HomeFragment.kt
@@ -3,8 +3,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ar.edu.ort.respirar.R
@@ -26,7 +26,7 @@ class HomeFragment : Fragment() {
         val buttonMap = view.findViewById<MaterialCardView>(R.id.mapButton)
         val buttonStations = view.findViewById<MaterialCardView>(R.id.stationsButton)
         val buttonFavorites = view.findViewById<MaterialCardView>(R.id.favoritesButton)
-        val buttonAboutUs = view.findViewById<MaterialCardView>(R.id.aboutUsButton)
+        val buttonAboutUs = view.findViewById<CardView>(R.id.aboutUsButton)
 
         buttonMap.setOnClickListener{
             findNavController().navigate(R.id.action_homeFragment_to_mapsFragment)
